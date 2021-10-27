d8bb209662674478c9b4e73955e4130fe0980d19
elemes <lasse.m.schwenger@gmail.com>
2021-10-28 00:20:40 +0200

    Config corrected. typo

--- STATQ/config.py
+++ STATQ/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    print(os.environ.get('DATABASE_URL')
+    print(os.environ.get('DATABASE_URL'))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
     FILE_UPLOADS = os.getcwd()+'static/files'
