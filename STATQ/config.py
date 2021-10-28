e66b827fa0475972b1660f999fcc07751189e350
elemes <lasse.m.schwenger@gmail.com>
2021-10-28 17:42:33 +0200

    Changed mail-service provider to sendgrid

--- STATQ/config.py
+++ STATQ/config.py
@@ -10,7 +10,7 @@ class Config:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
     ALLOWED_FILE_EXTENSIONS = ["CSV", "TXT"]
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
